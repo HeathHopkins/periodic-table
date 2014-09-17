fb44998a08af111add91ee13e59a63eae5bad53b
Heath Hopkins <dynamicdna@gmail.com>
2014-09-16 23:33:51 -0400

    remove scaffold comments

--- PeriodicTable/AppDelegate.cs
+++ PeriodicTable/AppDelegate.cs
@@ -7,32 +7,16 @@ using MonoTouch.UIKit;
 
 namespace PeriodicTable
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the
-    // User Interface of the application, as well as listening (and optionally responding) to
-    // application events from iOS.
     [Register("AppDelegate")]
     public partial class AppDelegate : UIApplicationDelegate
     {
-        // class-level declarations
         UIWindow window;
 
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            // create a new window instance based on the screen size
             window = new UIWindow(UIScreen.MainScreen.Bounds);
-			
-            // If you have defined a root view controller, set it here:
-            window.RootViewController = new PeriodicTableViewController();
-			
-            // make the window visible
-            window.MakeKeyAndVisible();
+			window.RootViewController = new PeriodicTableViewController();
+			window.MakeKeyAndVisible();
 			
             return true;
         }

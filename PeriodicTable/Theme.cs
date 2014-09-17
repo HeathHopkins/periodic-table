90f8c91d3dca4a0269eebf03e530fb3f7bc4e60b
Heath Hopkins <dynamicdna@gmail.com>
2014-09-16 23:32:36 -0400

    Add Theme File

---
+++ PeriodicTable/Theme.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace PeriodicTable
+{
+    public static class Theme
+    {
+        public static void Apply()
+        {
+
+        }
+        static Lazy<UIColor> backgroundColor = new Lazy<UIColor>(() => FlatUI.Color.PeterRiver);
+        public static UIColor BackgroundColor { get { return backgroundColor.Value; } }
+
+        //static Lazy<UIColor> linenPattern = new Lazy<UIColor>(() => UIColor.FromPatternImage(UIImage.FromFile("")));
+        //public static UIColor LinenPattern { get { return linenPattern.Value; } }
+
+        const string FontName = @"HelveticaNeue-Medium";
+        const string BoldFontName = @"HelveticaNeue-Bold";
+
+        public static UIFont FontOfSize(float size)
+        {
+            return UIFont.FromName(FontName, size);
+        }
+
+        public static UIFont BoldFontOfSize(float size)
+        {
+            return UIFont.FromName(BoldFontName, size);
+        }
+    }
+}
+
+
